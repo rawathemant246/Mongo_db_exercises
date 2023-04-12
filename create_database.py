1810cae37fee34141867d1803840081eff4cd64a
rawathemant246 <hemantrawat246@gmail.com>
2023-04-12 19:26:09 +0530

    updating create_database

--- create_database.py
+++ create_database.py
@@ -4,6 +4,11 @@ from pymongo import MongoClient
 client = MongoClient()
 client = MongoClient('mongodb://localhost:27017')
 
-def database():
+def database(db, collect):
     db =  client['wine_quality']
     collect = db['csv_file']
+    return collect
+
+a =12
+b =13
+
